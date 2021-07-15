e35af66afd3a6ccf19fcb18f5bdb757ca25f0ac3
ttzap <benvaknin742@gmail.com>
2021-07-15 19:13:22 -0400

    Add files via upload

---
+++ export all security groups and members.ps1
@@ -0,0 +1,31 @@
+﻿$Groups = (Get-AdGroup -filter * -SearchBase "OU=MNA-Sites,DC=MNA,DC=local" | Where {$_.name -like "**"} | select name -expandproperty name)
+
+
+$Table = @()
+
+$Record = [ordered]@{
+"Group Name" = ""
+"Name" = ""
+"Username" = ""
+}
+
+
+
+Foreach ($Group in $Groups)
+{
+
+$Arrayofmembers = Get-ADGroupMember -identity $Group | select name,samaccountname
+
+foreach ($Member in $Arrayofmembers)
+{
+$Record."Group Name" = $Group
+$Record."Name" = $Member.name
+$Record."UserName" = $Member.samaccountname
+$objRecord = New-Object PSObject -property $Record
+$Table += $objrecord
+
+}
+
+}
+
+$Table | export-csv "C:\scripts\results\SecurityGroups.csv" -NoTypeInformation
