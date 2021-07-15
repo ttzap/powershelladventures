e7861247fcd9f97c063cf73dc6fe266d45b287a0
ttzap <benvaknin742@gmail.com>
2021-07-15 19:05:14 -0400

    Create copyfiles.ps1

---
+++ copyfiles.ps1
@@ -0,0 +1,13 @@
+$userList = @('usera','userb')
+$rackSpaceU = "\\server-1\u-users$\"
+$destinationAzure = "E:\USERS\"
+
+
+
+ForEach ($item In $userList) { 
+
+        Copy-Item $rackspaceU$item -destination $destinationAzure -Recurse -Force
+        icacls "E:\users\$item" /grant $item":(OI)(CI)M" /T
+        Write-Host -ForegroundColor Green "$($item) User Drive Moved to Azure!" 
+        
+    }
